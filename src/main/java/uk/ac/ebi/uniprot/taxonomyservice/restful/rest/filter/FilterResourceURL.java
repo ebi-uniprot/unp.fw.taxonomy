e35589370fba6ff9759b82f1f1e791fb6fd36b3a
lgonzales <lgonzales@ebi.ac.uk>
2016-04-05 10:25:41 +0100

    - Update FilterResourceURL - Update Unit tests for FilterResourceURL

--- src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/rest/filter/FilterResourceURL.java
+++ src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/rest/filter/FilterResourceURL.java
@@ -1,20 +1,17 @@
 package uk.ac.ebi.uniprot.taxonomyservice.restful.rest.filter;
 
+import uk.ac.ebi.uniprot.taxonomyservice.restful.swagger.SwaggerConstant;
+
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
+import org.glassfish.jersey.server.ParamException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,57 +28,44 @@ public class FilterResourceURL implements ContainerRequestFilter {
 
     public static final Logger logger = LoggerFactory.getLogger(FilterResourceURL.class);
 
+    public static final String[] acceptedContentType = {MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON};
+
+    public static final String defaultContentType = MediaType.APPLICATION_JSON;
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        if (requestContext.getUriInfo() != null && requestContext.getUriInfo().getPath() != null) {
-            logger.debug("FilterResourceURL: " + requestContext.getUriInfo().getRequestUri().toString());
-            String path = requestContext.getUriInfo().getRequestUri().toString();
-            MultivaluedMap<String, String> queryString = requestContext.getUriInfo().getQueryParameters();
-            try {
-                if (requestContext.getUriInfo().getPath().endsWith(".xml")) {
-                    path = path.substring(0, path.indexOf(".xml"));
-                    logger.debug("new path: " + path);
+        String currentAcceptHeader = requestContext.getHeaders().getFirst(HttpHeaders.ACCEPT);
+        if (currentAcceptHeader == null || !isCurrentAcceptHeaderInAcceptedContentTypeList(currentAcceptHeader)) {
+            Optional<String> formatParamValue = getFormatParamFromRequestContext(requestContext.getUriInfo());
+            if(formatParamValue.isPresent()){
+                if(formatParamValue.get().equalsIgnoreCase("xml")){
                     requestContext.getHeaders().add(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML);
-                    requestContext.setRequestUri(getURI(path,queryString));
-                } else if (requestContext.getUriInfo().getPath().endsWith(".json")) {
-                    path = path.substring(0, path.indexOf(".json"));
-                    logger.debug("new path: " + path);
+                }else if(formatParamValue.get().equalsIgnoreCase("json")){
                     requestContext.getHeaders().add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON);
-                    requestContext.setRequestUri(getURI(path,queryString));
+                }else{
+                   throw new ParamException.QueryParamException(new IllegalArgumentException( SwaggerConstant
+                           .REQUEST_PARAMETER_INVALID_VALUE),"format","");
                 }
-            } catch (URISyntaxException e) {
-                logger.error("Error at FilterResourceURL: ",e);
+            }else{
+                requestContext.getHeaders().add(HttpHeaders.ACCEPT, defaultContentType);
             }
         }else{
-            logger.warn("FilterResourceURL: requestContext.getUriInfo is null");
+            logger.debug("FilterResourceURL, we already have a valid accept header: "+currentAcceptHeader);
         }
     }
 
-    private URI getURI(String path,  MultivaluedMap<String, String> queryString) throws URISyntaxException {
-        String absolutePath = path;
-        if(queryString!= null && !queryString.isEmpty()){
-            absolutePath += "?"+queryString.entrySet().stream()
-                    .flatMap(entry -> encodeMultiParameter(entry.getKey(), entry.getValue(), StandardCharsets.UTF_8))
-                    .reduce((param1, param2) -> param1 + "&" + param2)
-                    .orElse("");
+    private Optional<String> getFormatParamFromRequestContext(UriInfo uriInfo) {
+        Optional<String> format;
+        if(uriInfo != null && uriInfo.getQueryParameters() != null){
+            format = Optional.ofNullable(uriInfo.getQueryParameters().getFirst("format"));
+        }else{
+            format = Optional.empty();
         }
-        return new URI(absolutePath);
+        return format;
     }
 
-    private Stream<String> encodeMultiParameter(String key, List<String> values, Charset encoding) {
-        String[] arrayValues = values.toArray(new String[values.size()]);
-        return Stream.of(arrayValues).map(value -> encodeSingleParameter(key, value,encoding));
+    private boolean isCurrentAcceptHeaderInAcceptedContentTypeList(String currentAcceptHeader) {
+        return Stream.of(acceptedContentType).anyMatch(x -> x.equalsIgnoreCase(currentAcceptHeader));
     }
 
-    private String encodeSingleParameter(String key, String value, Charset encoding) {
-        return urlEncode(key, encoding) + "=" + urlEncode(value, encoding);
-    }
-
-    private String urlEncode(String value, Charset encoding) {
-        try {
-            return URLEncoder.encode(value, encoding.toString());
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalArgumentException("Cannot url encode " + value, e);
-        }
-    }
 }
--- src/test/java/uk/ac/ebi/uniprot/taxonomyservice/restful/rest/filter/FilterResourceURLTest.java
+++ src/test/java/uk/ac/ebi/uniprot/taxonomyservice/restful/rest/filter/FilterResourceURLTest.java
@@ -3,13 +3,12 @@ package uk.ac.ebi.uniprot.taxonomyservice.restful.rest.filter;
 import java.net.URI;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.*;
+import org.glassfish.jersey.server.ParamException;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -29,80 +28,121 @@ public class FilterResourceURLTest {
     }
 
     @Test
-    public void assertNullQueryInfoFilterParameterExceptionReturnNull() throws Exception{
-        ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
-        when(requestContext.getUriInfo()).thenReturn(null);
-
-        filterResourceURL.filter(requestContext);
-
-        assertNull(requestContext.getHeaders());
-    }
-
-    @Test
-    public void assertNullQueryPathFilterParameterExceptionReturnNull() throws Exception{
+    public void assertJsonFormatQueryParameterReturnJsonAcceptHeader() throws Exception{
         ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
         UriInfo mockedInfo = mock(UriInfo.class);
+        MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<>();
+        queryParameters.add("format","json");
 
         when(requestContext.getUriInfo()).thenReturn(mockedInfo);
-        when(requestContext.getUriInfo().getPath()).thenReturn(null);
+        when(requestContext.getUriInfo().getPath()).thenReturn("/taxonomy/id/12345?format=json");
+        when(requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParameters);
+        when(requestContext.getHeaders()).thenReturn(headers);
+
 
         filterResourceURL.filter(requestContext);
 
-        assertNull(requestContext.getHeaders());
+        headers.add(HttpHeaders.ACCEPT,MediaType.APPLICATION_JSON);
+        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT),is(equalTo(headers.get(HttpHeaders.ACCEPT))));
+    }
+
+
+    @Test
+    public void assertXmlFormatQueryParameterReturnXmlAcceptHeader() throws Exception{
+        ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
+        UriInfo mockedInfo = mock(UriInfo.class);
+        MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<>();
+        queryParameters.add("format","xml");
+
+        when(requestContext.getUriInfo()).thenReturn(mockedInfo);
+        when(requestContext.getUriInfo().getPath()).thenReturn("/taxonomy/id/12345?format=xml");
+        when(requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParameters);
+        when(requestContext.getHeaders()).thenReturn(headers);
+
+
+        filterResourceURL.filter(requestContext);
+
+        headers.add(HttpHeaders.ACCEPT,MediaType.APPLICATION_ATOM_XML);
+        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT),is(equalTo(headers.get(HttpHeaders.ACCEPT))));
     }
 
     @Test
-    public void assertJsonQueryPathFilterParameterExceptionReturnJsonAcceptHeader() throws Exception{
+    public void assertWithoutFormatAndAcceptHeaderParametersReturnDefaultJsonAcceptHeader() throws Exception{
         ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
         UriInfo mockedInfo = mock(UriInfo.class);
         MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
 
         when(requestContext.getUriInfo()).thenReturn(mockedInfo);
-        when(requestContext.getUriInfo().getPath()).thenReturn("/12345.json");
+        when(requestContext.getUriInfo().getPath()).thenReturn("/taxonomy/id/12345");
         when(requestContext.getHeaders()).thenReturn(headers);
 
-        when(requestContext.getUriInfo().getRequestUri()).thenReturn(new URI("/12345.json"));
+        when(requestContext.getUriInfo().getRequestUri()).thenReturn(new URI("/taxonomy/id/12345"));
 
         filterResourceURL.filter(requestContext);
 
-        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT), notNullValue());
-        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT).get(0), is(equalTo(MediaType.APPLICATION_JSON)));
+        headers.add(HttpHeaders.ACCEPT,MediaType.APPLICATION_JSON);
+        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT),is(equalTo(headers.get(HttpHeaders.ACCEPT))));
     }
 
     @Test
-    public void assertXmlQueryPathFilterParameterExceptionReturnJsonAcceptHeader() throws Exception{
+    public void assertValidJsonAcceptHeaderHasPriorityOverXmlFormatParamReturnJsonAcceptHeader() throws Exception{
         ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
         UriInfo mockedInfo = mock(UriInfo.class);
         MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+        headers.add(HttpHeaders.ACCEPT,MediaType.APPLICATION_JSON);
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<>();
+        queryParameters.add("format","xml");
 
         when(requestContext.getUriInfo()).thenReturn(mockedInfo);
-        when(requestContext.getUriInfo().getPath()).thenReturn("/12345.xml");
+        when(requestContext.getUriInfo().getPath()).thenReturn("/taxonomy/id/12345?format=xml");
+        when(requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParameters);
         when(requestContext.getHeaders()).thenReturn(headers);
 
-        when(requestContext.getUriInfo().getRequestUri()).thenReturn(new URI("/12345.xml"));
 
         filterResourceURL.filter(requestContext);
 
-        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT), notNullValue());
+        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT),is(equalTo(headers.get(HttpHeaders.ACCEPT))));
     }
 
     @Test
-    public void assertWithoutExtensionQueryPathFilterParameterExceptionReturnJsonAcceptHeader() throws Exception{
+    public void assertXmlFormatParamHasPriorityOverInvalidAtomAcceptHeaderAddXmlAcceptHeader() throws
+                                                                                                        Exception{
         ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
         UriInfo mockedInfo = mock(UriInfo.class);
         MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+        headers.add(HttpHeaders.ACCEPT,MediaType.APPLICATION_ATOM_XML);
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<>();
+        queryParameters.add("format","xml");
 
         when(requestContext.getUriInfo()).thenReturn(mockedInfo);
-        when(requestContext.getUriInfo().getPath()).thenReturn("/12345");
+        when(requestContext.getUriInfo().getPath()).thenReturn("/taxonomy/id/12345?format=xml");
+        when(requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParameters);
         when(requestContext.getHeaders()).thenReturn(headers);
 
-        when(requestContext.getUriInfo().getRequestUri()).thenReturn(new URI("/12345"));
 
         filterResourceURL.filter(requestContext);
 
-        assertNull(requestContext.getHeaders().get(HttpHeaders.ACCEPT));
+        headers.add(HttpHeaders.ACCEPT,MediaType.APPLICATION_XML);
+        assertThat(requestContext.getHeaders().get(HttpHeaders.ACCEPT),is(equalTo(headers.get(HttpHeaders.ACCEPT))));
     }
 
+    @Test(expected = ParamException.QueryParamException.class)
+    public void assertInvalidFormatParamThrowsQueryParamException() throws Exception{
+        ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
+        UriInfo mockedInfo = mock(UriInfo.class);
+        MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<>();
+        queryParameters.add("format","invalid");
+
+        when(requestContext.getUriInfo()).thenReturn(mockedInfo);
+        when(requestContext.getUriInfo().getPath()).thenReturn("/taxonomy/id/12345?format=invalid");
+        when(requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParameters);
+        when(requestContext.getHeaders()).thenReturn(headers);
+
+        filterResourceURL.filter(requestContext);
+    }
 
 
 }
