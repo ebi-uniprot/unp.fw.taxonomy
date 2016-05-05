8556abb2f7bb334a7b81f42356a3bd053641e393
lgonzales <lgonzales@ebi.ac.uk>
2016-05-05 17:59:39 +0100

    create taxonomy import

--- src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/rest/response/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ebi.ac.uk/uniprot/services/docs/xsd/taxonomy",
-        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package uk.ac.ebi.uniprot.taxonomyservice.restful.rest.response;
