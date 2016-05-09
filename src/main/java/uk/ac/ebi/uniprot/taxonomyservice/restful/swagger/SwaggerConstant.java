76cdfa5476c7d3c7548007136a4bff8d7c08079d
rantunes <rantunes@ebi.ac.uk>
2016-05-09 14:09:49 +0100

    Update API messages.

--- src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/swagger/SwaggerConstant.java
+++ src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/swagger/SwaggerConstant.java
@@ -1,54 +1,53 @@
 package uk.ac.ebi.uniprot.taxonomyservice.restful.swagger;
 
-/**
- * Created by lgonzales on 04/03/16.
- */
 public class SwaggerConstant {
 
-    public static final String TAXONOMY_API_VALUE = "Rest services to return information about protein taxonomy " +
-            "tree. Basically there are services that returns details about a taxonomy element and also details about " +
-            "it parent, siblings and children. There are also services that return the relationship between 2 " +
-            "taxonomy elements, and the path between them. Services can return JSON or XML format";
+    public static final String TAXONOMY_API_VALUE = "Rest services that return information about the taxonomy " +
+            "tree. The endpoints can return information on a given taxonomic node, on nodes that are related " +
+            "to a particular node, or provide a path of nodes from a specific start node to a specific end node." +
+            "The endpoints can return responses in an JSON or XML format.";
 
     //TAXONOMY BY ID
-    public static final String API_OPERATION_TAXONOMY_DETAIL_BY_ID = "This service return details about a taxonomy " +
-            "element, and also links for it parent, siblings and children ";
+    public static final String API_OPERATION_TAXONOMY_DETAIL_BY_ID = "The endpoint returns details on a given " +
+            "taxonomy element, and also links to its parent, siblings and direct children.";
 
-    public static final String NOTE_TAXONOMY_ID = "with taxonomy identification as parameter";
+    public static final String NOTE_TAXONOMY_ID = "With taxonomy identification as parameter.";
 
     //TAXONOMY PARENT
-    public static final String API_OPERATION_TAXONOMY_PARENT_BY_ID = "This service return details about a taxonomy " +
-            "parent element";
+    public static final String API_OPERATION_TAXONOMY_PARENT_BY_ID = "This endpoint returns details of the " +
+            "parent taxonomic node";
 
     //TAXONOMY SIBLINGS
-    public static final String API_OPERATION_TAXONOMY_SIBLINGS_BY_ID = "This service return a list of siblings that " +
-            "belongs to a taxonomy element";
+    public static final String API_OPERATION_TAXONOMY_SIBLINGS_BY_ID = "This service returns a list of siblings that " +
+            "belong to a taxonomy element";
 
     //TAXONOMY CHILDREN
-    public static final String API_OPERATION_TAXONOMY_CHILDREN_BY_ID = "This service return a list of children that " +
-            "belongs to a taxonomy element";
+    public static final String API_OPERATION_TAXONOMY_CHILDREN_BY_ID = "This service returns a list of children that " +
+            "belong to a taxonomy element";
+
     //TAXONOMY BY NAME
-    public static final String API_OPERATION_TAXONOMY_DETAIL_BY_NAME = "This service return a list of " +
-            "taxonomies with a specific name. it element contains taxonomy detail and also links for it parent, " +
-            "siblings and children";
+    public static final String API_OPERATION_TAXONOMY_DETAIL_BY_NAME = "This service returns a list of " +
+            "taxonomies with a specific name. Each element, within the list, contains the node's details, links to " +
+            "the parent, siblings and children";
 
     public static final String NOTE_TAXONOMY_DETAIL_BY_NAME = "with taxonomy name as parameter";
 
     //TAXONOMY RELATIONSHIP
-    public static final String API_OPERATION_TAXONOMY_RELATIONSHIP = "This service return the path between to " +
-            "taxonomies showing their relation";
+    public static final String API_OPERATION_TAXONOMY_RELATIONSHIP = "This service returns the path between two " +
+            "taxonomies showing their relationship";
 
     public static final String NOTE_TAXONOMY_RELATIONSHIP = "";
 
     // TAXONOMY PATH
-    public static final String API_OPERATION_TAXONOMY_PATH = "This service return all taxonomies elements " +
-            "that has relationship with taxonomyId in a specific direction (TOP or DOWN) and depth levels";
+    public static final String API_OPERATION_TAXONOMY_PATH = "This endpoint returns all taxonomy elements " +
+            "that are related with the taxonomic node, with the given taxonomyId for a specific direction (TOP or " +
+            "DOWN) and depth level";
 
     public static final String NOTE_TAXONOMY_PATH = "";
 
 
     //ERROR MESSAGES
-    public static final String API_RESPONSE_303 = "Taxonomy identification changed and new identification is now " +
+    public static final String API_RESPONSE_303 = "Taxonomy identifier has changed. New identifier is: " +
             "{newId}.";
 
     public static final String API_RESPONSE_404 = "Taxonomy entry not found.";
