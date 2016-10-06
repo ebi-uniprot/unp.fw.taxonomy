b05146b99b952675cec1ff7094782ec03012729c
lgonzales <lgonzales@ebi.ac.uk>
2016-10-06 16:25:48 +0100

    - code change for github migration

--- taxonomy-restful/src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/domain/package-info.java
+++ taxonomy-restful/src/main/java/uk/ac/ebi/uniprot/taxonomyservice/restful/domain/package-info.java
@@ -1,3 +1,3 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ebi.ac.uk/uniprot/services/docs/xsd/taxonomy",
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ebi.ac.uk/proteins/api/taxonomy/docs/xsd/taxonomy",
         elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package uk.ac.ebi.uniprot.taxonomyservice.restful.domain;
